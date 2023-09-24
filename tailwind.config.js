09a55ff5247efe0db7decdb8d0f327c0f9cdd9af
Akinniyi Ezekiel Wonderful <96802687+Wondersmasher@users.noreply.github.com>
2023-09-24 21:16:06 +0100

    added new themes color and sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      yellow: "#E1B168",
+      ash: "#DEDEDE",
+      white: "#ffffff",
+    },
+    fontSize: {
+      sm: "12px",
+      md: "14px",
+      lg: "16px",
+      "icon-sm": "20px",
+      "icon-md": "24px",
+      "icon-lg": "28px",
+    },
     extend: {},
   },
   plugins: [],
