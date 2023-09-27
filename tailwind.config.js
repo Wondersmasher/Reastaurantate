afb30accd2180d505565240ae15bec8e8773e054
Akinniyi Ezekiel Wonderful <96802687+Wondersmasher@users.noreply.github.com>
2023-09-27 14:16:54 +0100

    added more custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,42 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      yellow: "#E1B168",
-      ash: "#DEDEDE",
-      white: "#ffffff",
-    },
     fontSize: {
-      sm: "12px",
-      md: "14px",
-      lg: "16px",
-      "icon-sm": "20px",
-      "icon-md": "24px",
-      "icon-lg": "28px",
+      sm: "13px",
+      md: "15px",
+      lg: "17px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        yellow: "#E1B168",
+        ash: "#DEDEDE",
+        white: "#ffffff",
+        midblack: "#40464F",
+        black: "#292E36",
+      },
+      fontFamily: {
+        josefin: ["Josefin Sans", "sans-serif"],
+        cormorant: ["Cormorant Infant", "serif"],
+        zeyeda: ["Zeyada", "cursive"],
+      },
+      backgroundColor: {
+        "mid-black": "#292E36",
+        "light-pink": "#FFF8F5",
+        "pure-white": "#ffffff",
+      },
+      fontSize: {
+        "icon-sm": "20px",
+        "icon-md": "24px",
+        "icon-lg": "28px",
+        "h1-lg": "90px",
+        "h1-md": "70px",
+        "h1-sm": "60px",
+      },
+      aspectRatio: {
+        "2/1/": "2/1",
+        "3/2": "3/2",
+      },
+    },
   },
   plugins: [],
 };
