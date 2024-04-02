f11156e97df86cadf743eb5d61ebbd3d285f0ea4
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-02 20:59:39 +0100

    used cn in these files

--- src/components/generalcomponents/Button.jsx
+++ src/components/generalcomponents/Button.jsx
@@ -1,9 +1,13 @@
 import React, { memo } from "react";
+import { cn } from "../../lib/cn";
 
 const Button = ({ text, extra = "" }) => {
   return (
     <button
-      className={`border-[1px] ${extra} text-white border-yellow px-3 py-2 text-[12px] lg:text-[14px] hover:bg-yellow transition-colors duration-700 `}
+      className={cn(
+        `border-[1px] text-white border-yellow px-3 py-2 text-[12px] lg:text-[14px] hover:bg-yellow transition-colors duration-700 `,
+        extra
+      )}
     >
       {text}
     </button>
--- src/components/generalcomponents/Section.jsx
+++ src/components/generalcomponents/Section.jsx
@@ -1,11 +1,14 @@
 import React, { memo } from "react";
+import { cn } from "../../lib/cn";
 
 const Section = ({ children, bg, py = "py-16 md:py-24" }) => {
   return (
     <section
-      className={`${
-        bg === "p" ? "bg-light-pink" : bg === "w" ? "bg-white" : "bg-black"
-      } ${py} px-8 md:px-16 lg:px-32 flex  justify-center items-center`}
+      className={cn(
+        "px-8 md:px-16 lg:px-32 flex  justify-center items-center",
+        bg === "p" ? "bg-light-pink" : bg === "w" ? "bg-white" : "bg-black",
+        py
+      )}
     >
       {children}
     </section>
