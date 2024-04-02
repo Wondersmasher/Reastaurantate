f5aecccb5bfc502184f5d886b2c1fb16673d6923
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-02 20:58:58 +0100

    made images full width

--- src/components/aboutcomponents/Chefs.jsx
+++ src/components/aboutcomponents/Chefs.jsx
@@ -4,24 +4,27 @@ import { ourChef } from "../../assets/datas";
 
 const Chefs = () => {
   return (
-    <Section bg="p">
-      <div className="flex-1 flex flex-col items-center text-black max-w-7xl gap-2">
-        <p className="text-md border-y-[1px] border-black uppercase font-cormorant w-fit">
+    <Section bg='p'>
+      <div className='flex-1 flex flex-col items-center text-black max-w-7xl gap-2'>
+        <p className='text-md border-y-[1px] border-black uppercase font-cormorant w-fit'>
           Team
         </p>
-        <p className="text-sub-heading font-cormorant text-center">
+        <p className='text-sub-heading font-cormorant text-center'>
           Meet our professional chefs
         </p>
-        <div className="flex gap-4 flex-wrap lg:flex-nowrap justify-">
+        <div className='flex gap-4 flex-wrap lg:flex-nowrap justify-'>
           {ourChef.map((chef, id) => {
             return (
-              <div key={id} className="flex flex-col justify-center items-center flex-auto">
+              <div
+                key={id}
+                className='flex flex-col justify-center items-center flex-auto border-2 border-red-500'
+              >
                 <img
                   src={chef.img}
                   alt={chef.name}
-                  className=" bg-chef-bg aspect-square object-cover object-center"
+                  className=' bg-chef-bg aspect-square object-cover object-center w-full'
                 />
-                <p className="mt-2">{chef.name}</p>
+                <p className='mt-2'>{chef.name}</p>
                 <p>{chef.position}</p>
               </div>
             );
