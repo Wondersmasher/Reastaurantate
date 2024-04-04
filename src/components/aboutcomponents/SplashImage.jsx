717196e7be11f3726e9089beeb58ac627ba11647
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-04 20:28:53 +0100

    used cn in the file

--- src/components/aboutcomponents/SplashImage.jsx
+++ src/components/aboutcomponents/SplashImage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { cn } from "../../lib/cn";
 
 const SplashImage = ({ image }) => {
   return (
     <div
-      className={` ${image} object-cover object-center bg-no-repeat bg-cover py-24 md:py-32`}
+      className={cn(`object-cover object-center bg-no-repeat bg-cover py-24 md:py-32)`, image)}
     ></div>
   );
 };
